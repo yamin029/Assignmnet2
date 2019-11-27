13acf7ba3d35a426eacbea68c5525b828a89653c
yamin029 <yaminkhan017@gmail.com>
2019-11-27 21:46:40 +0600

    assignment 2

--- server.js
+++ server.js
@@ -2,15 +2,44 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.Server(app);
+const CURRENT_WORKING_DIR = process.cwd()
+var path = require('path');
+var bodyParser = require('body-parser')
+var socket = require('socket.io');
+var io = socket(server);
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+io.on('connection', function(socket){
+      console.log('A user connected');
+      socket.on('disconnect', function(){
+          console.log('User disconnected');
+      });
+  
+      socket.on('chat message', function(msg){
+          console.log('Message: '+msg);
+      });
+  
+      socket.on('chat message', function(msg){
+          io.emit('chat message', msg);
+      });
+  });
+  
+
+
+app.use('/public', express.static(path.join(CURRENT_WORKING_DIR, 'public')))
 
 app.get('/',function(request,response){
-      response.sendFile(__dirname + '/index.html')
+      response.sendFile(__dirname + '/views/index.html')
 })
 app.get('/add',function(request,response){
-      response.sendFile(__dirname + '/addnew.html')
+      response.sendFile(__dirname + '/views/addnew.html')
 })
 app.get('/cart',function(request,response){
-      response.sendFile(__dirname + '/cart.html')
+      response.sendFile(__dirname + '/views/cart.html')
+})
+app.get('/chat',function(request,response){
+      response.sendFile(__dirname + '/views/chat.html')
 })
 
 
